e4f45a9e3c8e394ded1c230fc1ff11d2c4301042
iKrelve <krelve@yeah.net>
2016-01-08 13:25:09 +0800

    添加库存检查

--- Application/Home/Controller/OrderController.class.php
+++ Application/Home/Controller/OrderController.class.php
@@ -106,6 +106,19 @@
 				}			
 			}
 		}
+		
+		public function queryGoodsNum(){
+			$id=$_POST["id"];
+			$goods=M("goods");
+			$result = $goods->where("id=10")->select();//[0]['goodsnum']
+			if($result == null){
+				echo(-1);
+			}else{
+				echo($result[0]['goodsnum']);
+			}
+		}
+		
+		
 		public function clearcart(){
 			$id=session("id");
 			if(isset($id)){
